f7768262d597cb155d1805e74b81f31f6aba8e5f
Sakaar Puri <sakaarpuri@users.noreply.github.com>
2026-03-10 14:48:10 +0000

    Add second batch of prompt vault thumbnails

--- src/data/promptCategories.js
+++ src/data/promptCategories.js
@@ -64,7 +64,7 @@ const CATEGORY_METADATA = {
 }
 
 const FALLBACK_TOOL = 'Runway'
-const GENERATED_THUMBNAIL_COUNT = 30
+const GENERATED_THUMBNAIL_COUNT = 60
 
 const toSlug = (value) =>
   String(value || '')
